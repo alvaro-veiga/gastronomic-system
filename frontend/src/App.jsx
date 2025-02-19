fb977595d5897b924aba0218066ba688a25982d8
Alvaro Veiga <alvarojoao17@gmail.com>
2025-02-19 19:04:56 -0300

    create popup, context and plate card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import Navbar from "./components/navbar/navbar"
 import { Outlet } from "react-router-dom"
 import Footer from "./components/footer/footer"
+import { CartProvider } from "./context/useCartContext"
 export default function App() {
 
   return (
     <>
+    <CartProvider>
       <Navbar/>
       <main>
         <Outlet/>
       </main>
       <Footer/>
+    </CartProvider>
     </>
   )
 }
---
+++ frontend/src/context/useCartContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext()
+
+export function CartProvider({children}) {
+    
+    const [cartItems, setCartItems] = useState([])
+
+    const addToCart = (itemToAdd) => {
+        const checkItemAlready = cartItems.find((cartItems) => {
+            return cartItems._id === itemToAdd._id
+        })
+        if (!checkItemAlready) {
+            setCartItems([...cartItems, itemToAdd])
+            console.log('item adicionado com sucesso')
+        } else {
+            console.log('O item ja esta no seu carrinho')
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+
+    }
+
+    return(
+        <CartContext.Provider value={{ removeFromCart, addToCart, cartItems}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export const useCartContext = () => {
+    const context = useContext(CartContext)
+
+    if (!context) {
+        console.log('You are out of CartContext')
+    }
+
+    return context
+}
