3e3bfa6837657dc37aa4523c7ee23afec0daf59d
Alvaro Veiga <alvarojoao17@gmail.com>
2025-02-19 13:20:00 -0300

    create new service

--- frontend/src/pages/plates/page.jsx
+++ frontend/src/pages/plates/page.jsx
@@ -1,4 +1,21 @@
+import platesServices from "../../services/plates"
+import { useEffect } from "react";
+
 export default function Plates() {
+    const { getAvailablePlates, platesList, platesLoading, refetchPlates} = platesServices()
+
+    useEffect(() => {
+            if (refetchPlates) {
+                getAvailablePlates()
+            }
+        }, [refetchPlates]);
+    
+        if (orderLoading) {
+            return( <Loading/>)
+        }
+
+        console.log(platesList)
+
     return(
         <h1>Plates</h1>
     )
